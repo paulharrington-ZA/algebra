cdd80cba06ebd3f275ea4f5eebbdc454293751c9
Paul Harrington <development@paulharrington.co.za>
2019-10-14 09:41:17 +0200

    code clean up

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -24,151 +24,26 @@ namespace ConsoleApplication3
             matrix[2, 1] = 2;
             matrix[2, 2] = 4;
             matrix[2, 3] = 6;
-            var leading = FindLeadingVar(matrix);
+            var leading = Matrices.FindLeadingVar(matrix);
             Console.WriteLine("Original Matrix");
-            PrintMatrix(matrix);
+            Matrices.PrintMatrix(matrix);
             Console.WriteLine($"Leading Variable is in row: { leading}");
             if (leading > 0)
-                SwitchRow(matrix, leading, 0);
+                Matrices.SwitchRow(matrix, leading, 0);
             Console.WriteLine("After Sort");
-            PrintMatrix(matrix);
+            Matrices.PrintMatrix(matrix);
             Console.ReadKey();
-            Gaussian(matrix);
+            Matrices.Gaussian(matrix);
             Console.WriteLine("After Gausian");
-            PrintMatrix(matrix);
+            Matrices.PrintMatrix(matrix);
             Console.ReadKey();
 
-            Jordan(matrix);
+            Matrices.Jordan(matrix);
             Console.WriteLine("After Gauss-Jordan");
-            PrintMatrix(matrix);
+            Matrices.PrintMatrix(matrix);
             Console.ReadKey();
         }
 
-        static void PrintMatrix(float[,] matrix)
-        {
-            var m = matrix.GetLength(0);
-            var n = matrix.GetLength(1);
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(matrix[i,j]);
-                    if (j < n - 1) Console.Write(", ");
-                }
-                Console.WriteLine();
-            }
-        }
-        static int FindLeadingVar(float[,] matrix)
-        {
-            var leading = new Tuple<int, int>(0,0);
-            var m = matrix.GetLength(0);
-            var n = matrix.GetLength(1);
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (!matrix[i, j].Equals(0) && j <= leading.Item2)
-                    {
-                        leading = new Tuple<int, int>(i,j);
-                        i++;
-                        break;
-                    }
-                }
-            }
-            return leading.Item1;
-        }
-
-        static void SwitchRow(float[,] matrix, int source, int destination)
-        {
-            var m = matrix.GetLength(1);
-            
-            for (int i = 0; i < m; i++)
-            {
-                var ce = matrix[destination, i];
-                matrix[destination, i] = matrix[source, i];
-                matrix[source, i] = ce;
-            }
-        }
-
-        static void AddRow(float[,] matrix, int source, int destination, float factor)
-        {
-            // get number of columns
-            var n = matrix.GetLength(1);
-            // iterate through columns
-            for (var i = 0; i < n; i ++)
-            {
-                // get multiple
-                float multiple = 0f - ((1f/factor)*matrix[source, i]);
-                // add to destination
-                matrix[destination, i] = matrix[destination, i] + multiple;
-            }
-
-        }
-
-        static void Gaussian(float[,] matrix)
-        {
-            var m = matrix.GetLength(0);
-            var n = matrix.GetLength(1);
-            
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (matrix[i, j].Equals(1)) break;
-                    
-                    if (!matrix[i, j].Equals(0))
-                    {
-                        var source = matrix[i, j];
-                        if (!source.Equals(1))
-                        {
-                            AddRow(matrix,i,i,source);
-                        }
-                        // reassign source incase it changed
-                        source = matrix[i, j];
-                        for (int p = i + 1; p < m; p++)
-                        {
-                            if (p > m) break;
-                            if (!matrix[p,j].Equals(0))
-                            {
-                                var target = matrix[p,j];
-                                var factor = source/target;
-                                AddRow(matrix,i,p,factor);
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-
-        static void Jordan(float[,] matrix)
-        {
-            var m = matrix.GetLength(0);
-            var n = matrix.GetLength(1);
-
-            for (int i = m - 1; i > -1; i--)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (!matrix[i, j].Equals(0))
-                    {
-                        var source = matrix[i, j];
-                        for (int p = i - 1; p > -1; p--)
-                        {
-                            if (p < 0) break;
-                            if (!matrix[p, j].Equals(0))
-                            {
-                                var target = matrix[p, j];
-                                var factor = source / target;
-                                AddRow(matrix, i, p, factor);
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-        }
+ 
     }
 }
